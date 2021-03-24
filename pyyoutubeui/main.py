d07e3014d226d6ef799231190ba35253a3503cbd
pmathivanan <pmathivanan@logitech.com>
2021-03-24 16:54:10 +0530

    Add styles

--- pyyoutubeui/main.py
+++ pyyoutubeui/main.py
@@ -1,5 +1,6 @@
 from PyQt5 import  QtWidgets as Widgets
 from PyQt5 import QtCore
+import qtawesome as qta
 import sys
 import os
 
@@ -9,7 +10,7 @@ class MainWindow(Widgets.QWidget):
         
         super().__init__()
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint) 
-        self.setWindowTitle("Youtube Video Downloader")
+        self.setWindowTitle("PyTube Download")
         self.dragPos = QtCore.QPoint()
         self.initUI()
         
@@ -17,23 +18,33 @@ class MainWindow(Widgets.QWidget):
     def initUI(self) -> None:
        
         self._set_window_geometry()
-        self.container_frame = Widgets.QFrame(self)
-        self.footer_frame = Widgets.QFrame(self)
+        self.container = Widgets.QFrame(self)
         self._toolbar_ui()
+        self.container.resize(self.width(), 15 + self.height() - self.toolbar.height())
+        self.container.move(0, self.toolbar.height())
+        self._footer_ui()
         self._load_style_sheet()
-    
+        
     def _toolbar_ui(self):
-        self.toolbar_frame = Widgets.QFrame(self)
-        self.toolbar_frame.setObjectName("toolbar_frame")
-        self.toolbar_frame.setFixedWidth(self.width())
-        self.toolbar_frame.setFixedHeight(60)
-        self.toolbar_frame.mouseMoveEvent = self.mouseMoveEvent_
-        self.toolbar_frame.mousePressEvent = self.mousePressEvent_
+        self.toolbar = Widgets.QFrame(self)
+        self.toolbar.setObjectName("toolbar")
+        self.toolbar.resize(self.width(), 65)
+        self.toolbar.mouseMoveEvent = self._toolbar_mouse_move_event
+        self.toolbar.mousePressEvent = self._toolbar_mouse_press_event
+        title_label = Widgets.QLabel('[ PyTube Downloader ]', self.toolbar)
+        title_label.setObjectName('title_label')
+        title_label.move(20, 15)
+        
+        
+    def _footer_ui(self):
+        self.footer = Widgets.QFrame(self)
+        self.footer.setObjectName("footer")
+        self.footer.resize(self.width(), self.height() - self.container.height())
+        self.footer.move(0, self.container.height())
+        
         
     def _load_style_sheet(self) -> None:
-        """
-        Read the extrnal styles.css file and set the styles
-        """
+       
         file_path = os.path.join(os.path.dirname(__file__), 'styles.css')
         with open(file_path, 'r') as file:
             self.setStyleSheet(file.read())
@@ -47,10 +58,10 @@ class MainWindow(Widgets.QWidget):
         x, y = int(size.width() * 0.17), int(size.height() * 0.04)
         self.setGeometry(x, y, width, height)
     
-    def mousePressEvent_(self, event):                                 # +
+    def _toolbar_mouse_press_event(self, event): 
         self.dragPos = event.globalPos()
         
-    def mouseMoveEvent_(self, event):                                  # !!!
+    def _toolbar_mouse_move_event(self, event):
         if event.buttons() == QtCore.Qt.LeftButton:
             self.move(self.pos() + event.globalPos() - self.dragPos)
             self.dragPos = event.globalPos()
